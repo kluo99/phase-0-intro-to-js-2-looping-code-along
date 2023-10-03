b36d0df31a72fba7743873131d0f278c4028c553
Kenneth <luo.kenneth1999@gmail.com>
2023-10-02 22:08:54 -0400

    Finished Lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(array, eventName) {
+    const newArray = [];
+    for (let i = 0; i < array.length; i++) {
+        newArray.push(`Thank you, ${array[i]}, for the wonderful ${eventName} gift!`);
+    } 
+    return newArray; 
+}
+
+function countDown(n) {
+    console.log(n);
+    while(n > 0) {
+        n--;
+        console.log(n);
+    }
+}
+console.log(countDown(10));
